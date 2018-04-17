e3cce9a5964c4ceb7c368f69299181403d9bfac0
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-04-16 20:29:29 -0600

    added purpose, audience, goal and persona

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Jullyane Hawkins' Page</title>
+	</head>
+	<body>
+		<h1>Purpose</h1>
+		<p>This project will be a professional portfolio website with focus on full stack web development.</p>
+		<h1>Audience</h1>
+		<p>Web development audience such as startup founders, hiring managers and recruiters.</p>
+		<h1>Goal</h1>
+		<p>Get employers to contact me by showing them my work through the website.</p>
+		<h1>Persona</h1>
+		<p><strong>Name: </strong>John Mierzwa</p>
+		<p><strong>Age: </strong>53</p>
+		<p><strong>Profession: </strong>CEO & Founder of Ingenuity Software Labs, a software development firm specialized on custom software and mobile app development, strategic consultation, and web design and development.</p>
+		<p><strong>Technology: </strong>Huge Apple fan. Uses an iMac Pro (4TB storage) at work, a MacBook Pro at home, an iPad Pro (12.9” screen) while traveling and always has his iPhone X (256GB of capacity) and his Apple Watch Series 3 (GPS + Cellular) with him.</p>
+		<p><strong>Attitudes and Behaviors: </strong>As a successful businessman, John has a very tight schedule. His networking is large, and he is very active in the tech community in Albuquerque, NM. He is always looking for new talents to be a part of his team at his company. One way that he connects with people is through LinkedIn and Twitter. But he also wants to see other projects from people. That’s why he takes a few moments to look into web development portfolios.</p>
+		<p><strong>Frustrations and Needs: </strong>John is looking to hire a full stack developer intern to be part of his company ASAP, but he is having trouble on finding a qualified candidate that can accept his limit benefits offer.</p>
+		<p><strong>Goals: </strong>John wants to find a good fit for his company.</p>
+		<p><strong>User Story: </strong>As a user, John wants to find a potential candidate for the Full Stack Web Development Internship at his company.</p>
+	</body>
+</html>
