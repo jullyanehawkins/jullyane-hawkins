53a92cbaa00108cd45c58b1729183fee1b0a1703
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-04-25 16:27:30 -0600

    added my content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,5 +5,24 @@
 		<title>Jully's PWP Milestone 2α</title>
 	</head>
 	<body>
+		<h1>Content Strategy</h1>
+		<h3>Header:</h3>
+		<p>This section will be the first thing the user sees. It will be clean and simple: my name, my role and some sort of desk picture.</p>
+		<h3>About Me:</h3>
+		<p>This section will be a personal introduction. I will include a professional photo of myself and my elevator pitch.</p>
+		<h3>Portfolio:</h3>
+		<p>This section will show past projects that I was part of, with the potential to add more examples as my projects increases.</p>
+		<h3>Contact:</h3>
+		<p>This section will be a simple contact form. Where people can conatct me filling out their names, email and message.</p>
+		<h3>Footer:</h3>
+		<p>This section will include social media icons that link to my GitHub, LinkedIn and Twitter pages.</p>
+		<br>
+		<h1>Wireframes</h1>
+		<h3>Mobile Layout</h3>
+		<img src="" alt="Mobile Layout">
+		<br>
+		<h3>Full Screen Layout</h3>
+		<img src="" alt="Full Screen Layout">
+		<br>
 	</body>
 </html>
