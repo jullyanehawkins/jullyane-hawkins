64a514a592cc3254e6542feb045fa96fe73cb65d
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-05-19 22:24:18 -0600

    Why my background image is not showing? :weary:

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+		<link rel="stylesheet" href="style.css" type="text/css">
 		<title>My Portfolio</title>
 	</head>
 	<body>
@@ -11,23 +12,27 @@
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
-				<div class="collapse navbar-collapse" id="navbarNav">
+				<div class="collapse navbar-collapse p-3 mb-2 bg-success text-white" id="navbarNav">
 					<ul class="nav navbar-nav ml-auto">
 						<li class="nav-item active">
-							<a class="nav-link" href="#home">Home</a>
+							<a class="nav-link text-white" href="#home">Home</a>
 						</li>
 						<li class="nav-item active">
-							<a class="nav-link" href="#about me">About Me</a>
+							<a class="nav-link text-white" href="#about-me">About Me</a>
 						</li>
 						<li class="nav-item active">
-							<a class="nav-link" href="#portfolio">Portfolio</a>
+							<a class="nav-link text-white" href="#portfolio">Portfolio</a>
 						</li>
 						<li class="nav-item active">
-							<a class="nav-link" href="#contact-section">Contact</a>
+							<a class="nav-link text-white" href="#contact">Contact</a>
 						</li>
 					</ul>
 				</div>
 			</nav>
 		</header>
+		<section>
+			<h1 class="display-3 text-center font-weight-bold co">Jullyane Hawkins</h1>
+			<p class="lead text-center">Web Development & Hospitality Management</p>
+		</section>
 	</body>
 </html>
