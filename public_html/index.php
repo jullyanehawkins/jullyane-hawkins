1f7a1fc966d704b16761f3bb72372dc321059d68
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-05-19 21:54:31 -0600

    Created navbar

--- public_html/index.php
+++ public_html/index.php
@@ -3,9 +3,31 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-		<title>Jullyane Hawkins' Portfolio</title>
+		<title>My Portfolio</title>
 	</head>
 	<body>
-
+		<header>
+			<nav class="navbar navbar-expand-md navbar-light bg-light d-flex justify-content-end">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="nav navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="#home">Home</a>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link" href="#about me">About Me</a>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link" href="#portfolio">Portfolio</a>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link" href="#contact-section">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
 	</body>
 </html>
