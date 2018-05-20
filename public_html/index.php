99f12e788f9713cdfc69b1aa6e87a02e567d4608
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-05-19 22:42:59 -0600

    Now why my background image is showing twice? :weary:

--- public_html/index.php
+++ public_html/index.php
@@ -12,27 +12,46 @@
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
-				<div class="collapse navbar-collapse p-3 mb-2 bg-success text-white" id="navbarNav">
+				<div class="collapse navbar-collapse" id="navbarNav">
 					<ul class="nav navbar-nav ml-auto">
 						<li class="nav-item active">
-							<a class="nav-link text-white" href="#home">Home</a>
+							<a class="nav-link" href="#home">Home</a>
 						</li>
 						<li class="nav-item active">
-							<a class="nav-link text-white" href="#about-me">About Me</a>
+							<a class="nav-link" href="#about-me">About Me</a>
 						</li>
 						<li class="nav-item active">
-							<a class="nav-link text-white" href="#portfolio">Portfolio</a>
+							<a class="nav-link" href="#portfolio">Portfolio</a>
 						</li>
 						<li class="nav-item active">
-							<a class="nav-link text-white" href="#contact">Contact</a>
+							<a class="nav-link" href="#contact">Contact</a>
 						</li>
 					</ul>
 				</div>
 			</nav>
 		</header>
 		<section>
-			<h1 class="display-3 text-center font-weight-bold co">Jullyane Hawkins</h1>
-			<p class="lead text-center">Web Development & Hospitality Management</p>
+			<div class="row-fluid">
+				<div class="column">
+					<div class="container pt-5">
+			<h1 class="display-3 text-center font-weight-bold co text-white">Jullyane Hawkins</h1>
+			<p class="lead text-center text-white">Web Development & Hospitality Management</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="py-5">
+			<div class="container" id="about-me">
+				<div class="row">
+					<div class="column-fluid col-md-4">
+						<h2>About Jully</h2>
+						<img class="img-fluid rounded-circle" src="jullyane-hawkins.jpg" alt="Jullyane Hawkins">
+					</div>
+					<div class="column-fluid mt-5 col 2">
+						<p class="lead">Jully is originally from Recife, PE - Brazil. *I WILL WRITE MORE LATER* Connect with her on <a href="https://www.linkedin.com/in/jullyanehawkins/" target="_blank">LinkedIn</a> or on <a href="https://twitter.com/jullyanehawkins" target="_blank">Twitter.</a></p>
+					</div>
+				</div>
+			</div>
 		</section>
 	</body>
 </html>
